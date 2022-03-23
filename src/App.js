7b16118c12b02ef2c55dd81b9fa52b6d9ab6eb3e
Aboulaye Tchakoura <aboulayetchakoura@Aboulayes-Air.home>
2022-03-23 15:39:47 -0400

    added linked

--- src/App.js
+++ src/App.js
@@ -1,92 +1,104 @@
 // import React , { Components} from 'react'
-import './App.css';
-import { Layout, Header, Navigation,Drawer,Content } from 'react-mdl';
-import { Link } from 'react-router-dom';
-import photo from './Components/abdel.jpeg';
-import Main from './Components/Main'
+import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import { Link } from "react-router-dom";
+import photo from "./Components/abdel.jpeg";
+import Main from "./Components/Main";
 // import About from './Components/About';
 // import Resume from './Components/Resume';
 // import Project from './Components/Project';
 // import Contact from './Components/Contact';
 // import Home from './Components/Home';
 
-
-import { Cell, Grid } from 'react-mdl';
-
+import { Cell, Grid } from "react-mdl";
 
 function App() {
   return (
     <div className="App">
-      
       <header className="App-header">
-      <Layout>
-        <Header  className = 'header' title="Title" scroll>
+        <Layout>
+          <Header className="header" title="Title" scroll>
             <Navigation>
-            <Link to ="/">Home</Link>
-                <Link to ="/About">About</Link>
-                <Link to ="/Resume">Resume</Link>
-                <Link to ="/Project">Project</Link>
-                <Link to ="/Contact">Contact</Link>
+              <Link to="/">Home</Link>
+              <Link to="/About">About</Link>
+              <Link to="/Resume">Resume</Link>
+              <Link to="/Project">Project</Link>
+              <Link to="/Contact">Contact</Link>
             </Navigation>
-        </Header>
-        <Drawer title="Title">
+          </Header>
+          <Drawer title="Title">
             <Navigation>
-                <Link to ="/">Home</Link>
-                <Link to ="/About">About</Link>
-                <Link to ="/Resume">Resume</Link>
-                <Link to ="/Project">Project</Link>
-                <Link to ="/Contact">Contact</Link>
+              <Link to="/">Home</Link>
+              <Link to="/About">About</Link>
+              <Link to="/Resume">Resume</Link>
+              <Link to="/Project">Project</Link>
+              <Link to="/Contact">Contact</Link>
             </Navigation>
-            
-        </Drawer>
-        <Content>
+          </Drawer>
+          <Content>
             <div className="page-content" />
 
-            <Main/>
+            <Main />
 
-            <div className = "cont" style ={ {width : '100%', margin : 'auto'}}>
-                <Grid className = 'content'>
-                      <Cell col = {10}> 
-                      {/* <div id = "photo-container" > */} 
-                        <img  id = "photo"
-                        //  src ='https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_client_male_person_user_work_sport_beard_team_glasses-512.png'
-                        //  alt="avator" />
+            <div className="cont" style={{ width: "100%", margin: "auto" }}>
+              <Grid className="content">
+                <Cell col={10}>
+                  {/* <div id = "photo-container" > */}
+                  <img
+                    id="photo"
+                    //  src ='https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_client_male_person_user_work_sport_beard_team_glasses-512.png'
+                    //  alt="avator" />
 
-                        src ={photo} alt =''/> 
-                          {/* </div> */}
-                        <h2>A.Tchakoura</h2>
-                    </Cell>
-                </Grid>
+                    src={photo}
+                    alt=""
+                  />
+                  {/* </div> */}
+                  <h2>A.Tchakoura</h2>
+                </Cell>
+              </Grid>
             </div>
-            <div className = 'text-banner'>
+            <div className="text-banner">
+              <h1>Full Stack Web Developer</h1>
+              <hr />
+              <p>
+                {" "}
+                HTML/CSS | Bootstrap | JavaScript | React | React Native |
+                NodeJs | Express{" "}
+              </p>
+              <div className="Social_Links">
+                <a
+                  href="https://github.com/abdel1413"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+                <a
+                  href="linkedin.com/in/aboulaye-tchakoura/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
 
-                <h1>
-                  Full Stack Web Developer
-                </h1>
-                <hr/>
-                <p> HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJs | Express  </p>
-                <div className ='Social_Links'>
-                    <a  href ='https://github.com/abdel1413' rel = 'noopener noreferrer' >
-                      <i className = 'fa fa-github-square' aria-hidden = 'true'/>
-                    </a>
-                    <a  href ='http//google.com' rel = 'noopener noreferrer' target = '_blank' >
-                      <i className = 'fa fa-linkedin-square' aria-hidden = 'true'/>
-                    </a>
-                    
-                    <a  href ='http//google.com' rel = 'noopener noreferrer' target = '_blank' >
-                      <i className = 'fa fa-facebook-square' aria-hidden = 'true'/>
-                    </a>
+                <a
+                  href="http//google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-facebook-square" aria-hidden="true" />
+                </a>
 
-                    <a  href ='http//google.com' rel = 'noopener noreferrer' target = '_blank' >
-                      <i className = 'fa fa-youtube-square' aria-hidden = 'true'/>
-                    </a>
-
-                </div>
+                <a
+                  href="http//google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-youtube-square" aria-hidden="true" />
+                </a>
+              </div>
             </div>
-            
-        </Content>
-    </Layout>
-
+          </Content>
+        </Layout>
       </header>
     </div>
   );
