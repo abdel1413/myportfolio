2411868fb5b5839a330b65a3d17e8a79618c588e
Aboulaye Tchakoura <aboulayetchakoura@Aboulayes-Air.home>
2022-03-23 17:54:51 -0400

    added resume pic

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import resume from "./Aboulaye_Tchakoura.pdf";
+import resume_png from "./Aboulaye_Tchakoura.png";
 
 class Resume extends Component {
   render() {
     return (
       <div id="my-resume">
+        <img src={resume_png} alt="" />
         <a href={resume} rel="noopener noreferrer" target="_blank">
           {" "}
           Download
