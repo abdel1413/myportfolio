da46375728a469ec00fa87718c1fa77bb6c5bacb
Aboulaye Tchakoura <aboulayetchakoura@Aboulayes-Air.home>
2022-03-23 15:30:06 -0400

    aded

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,16 +1,17 @@
-import React ,{Component} from 'react';
-import resume from './Aboulaye_Resume.pdf'
+import React, { Component } from "react";
+import resume from "./Aboulaye_Tchakoura.pdf";
 
-class Resume extends Component{
-    render(){
-        return(
-            <div id ="my-resume">
-                <a href = {resume} rel = 'noopener noreferrer' target = "_blank"> Download</a>
-            </div>
-        )
-    }
+class Resume extends Component {
+  render() {
+    return (
+      <div id="my-resume">
+        <a href={resume} rel="noopener noreferrer" target="_blank">
+          {" "}
+          Download
+        </a>
+      </div>
+    );
+  }
 }
 
-export default Resume ;
-
- 
+export default Resume;
